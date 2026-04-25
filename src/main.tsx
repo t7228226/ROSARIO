101f26e11b20a04c1ec56844a5834584751470ca
t7228226 <t7228226@gmail.com>
2026-04-26 07:08:22 +0800

    Enable safe schedule color labels

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./schedule-overrides.css";
 // import { installScheduleRuntime } from "./schedule-runtime";
 import { installScheduleShareRuntime } from "./schedule-share-runtime";
 import { installScheduleTipRuntime } from "./schedule-tip-runtime";
+import { installScheduleColorRuntime } from "./schedule-color-runtime";
 
 declare global {
   interface Window {
@@ -129,4 +130,5 @@ try {
 
 // window.setTimeout(() => installOptionalRuntime("站點試排外掛", installScheduleRuntime), 500);
 window.setTimeout(() => installOptionalRuntime("站點浮動提示窗", installScheduleTipRuntime), 550);
+window.setTimeout(() => installOptionalRuntime("站點顏色標籤", installScheduleColorRuntime), 650);
 window.setTimeout(() => installOptionalRuntime("站點分享外掛", installShareRuntimeWithoutFilterListener), 800);
